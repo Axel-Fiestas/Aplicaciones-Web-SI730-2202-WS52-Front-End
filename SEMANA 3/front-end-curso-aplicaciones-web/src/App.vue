6674ae02f5dff61ba83e318d8aef9d191d1bfec0
Axekran <josue123452002@gmail.com>
2022-09-02 21:41:06 -0500

    npm run build hecho

--- SEMANA 3/front-end-curso-aplicaciones-web/src/App.vue
+++ SEMANA 3/front-end-curso-aplicaciones-web/src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="Eso tilín!" />
+      <HelloWorld msg="Hello my friend!" />
     </div>
   </header>
 
@@ -45,3 +45,4 @@ header {
   }
 }
 </style>
+
