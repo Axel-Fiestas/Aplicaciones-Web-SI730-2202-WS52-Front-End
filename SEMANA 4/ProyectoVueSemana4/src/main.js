521db8c8da77d30a24d2fb74bf66a0266a26ddde
Axekran <josue123452002@gmail.com>
2022-09-06 20:00:10 -0500

    Boton añadido

--- SEMANA 4/ProyectoVueSemana4/src/main.js
+++ SEMANA 4/ProyectoVueSemana4/src/main.js
@@ -1,6 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import PrimeVue from 'primevue/config';
 import './assets/main.css'
+import Button from 'primevue/button';
 
-createApp(App).mount('#app')
+import 'primevue/resources/themes/saga-green/theme.css'       //theme
+import 'primevue/resources/primevue.min.css'            //core css
+import 'primeicons/primeicons.css'                           //icons
+
+
+const app = createApp(App);
+app.use(PrimeVue);
+app.component('Button', Button);
+
+app.mount('#app')
+
