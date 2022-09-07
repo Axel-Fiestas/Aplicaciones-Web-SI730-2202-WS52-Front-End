e008ed4d819a39daab54f7cc56e819616a4d1762
Axekran <josue123452002@gmail.com>
2022-09-06 21:43:04 -0500

    Formulario hecho

--- SEMANA 4/ProyectoVueSemana4/src/App.vue
+++ SEMANA 4/ProyectoVueSemana4/src/App.vue
@@ -1,11 +1,103 @@
-<script setup>
+<script>
+
+  export default {
+    data() {
+	    return {
+	    	selectedCity: null,
+	    	cities: [
+	    		{name: 'New York', code: 'NY'},
+	    		{name: 'Rome', code: 'RM'},
+	    		{name: 'London', code: 'LDN'},
+	    		{name: 'Istanbul', code: 'IST'},
+	    		{name: 'Paris', code: 'PRS'}
+	    	]
+	    }
+    }
+
+  }
 
 </script>
 
 <template>
 
-  <div>Hi</div>
-  <Button label="Submit" icon="pi pi-check" class="p-button-outlined p-button-sm"></Button>
+  <form>
+
+
+    <div class="card">
+
+    <div class="field grid">
+
+
+        <!--Codigo-->
+        <label for="code" class="col-12 mb-2 md:col-2 md:mb-0">Codigo*</label>
+
+        <div class="col-12 md:col-10">
+          <InputText id="code"></InputText>
+        </div>
+        
+        <Button label="Validar" class="p-button-sm"></Button>
+
+    </div>
+
+    <div class="field grid"></div>
+        <!--Nombre-->
+        <label for="name" class="col-12 mb-2 md:col-2 md:mb-0">Nombre*</label>
+
+        <div class="col-12 md:col-10">
+
+          <InputText placeholder="Solo letras y números" id="name"></InputText>
+
+        </div>
+        
+
+    </div>
+
+    <div class="field grid">
+      <!--Precio-->
+      <label for="price" class="col-12 mb-2 md:col-2 md:mb-0">Precio*</label>
+
+      <div class="col-12 md:col-10">
+
+        <InputNumber v-model="value3" mode="decimal" showButtons buttonLayout="vertical"
+            decrementButtonClass="p-button-secondary" incrementButtonClass="p-button-secondary" incrementButtonIcon="pi pi-plus" decrementButtonIcon="pi pi-minus"></InputNumber>
+
+      </div>
+      
+
+    </div>
+
+    <div class="field grid">
+      <!--telefono-->
+
+      <div class="col-12 md:col-10">
+
+        <label for="telefono" class="col-12 mb-2 md:col-2 md:mb-0">Telefono*</label>
+        <InputMask id="telefono" v-model="value" mask="a9-99999a"></inputMask>
+
+      </div>
+      
+    
+    </div>
+
+
+    <div class="field grid">
+      <!--Precio-->
+
+      <div class="col-12 md:col-10">
+
+        <label for="categoria" class="col-12 mb-2 md:col-2 md:mb-0">Categoria</label>
+        <pv-dropdown id="cagetoria" :options="cities" optionLabel="name" placeholder="Select a Category"></pv-dropdown>
+
+      </div>
+      
+    
+    </div>
+
+
+
+  </form>
+
+
 
 </template>
 
--- SEMANA 4/ProyectoVueSemana4/src/main.js
+++ SEMANA 4/ProyectoVueSemana4/src/main.js
@@ -3,15 +3,26 @@ import App from './App.vue'
 import PrimeVue from 'primevue/config';
 import './assets/main.css'
 import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
+import InputNumber from 'primevue/inputnumber';
+import Dropdown from 'primevue/dropdown';
 
-import 'primevue/resources/themes/saga-green/theme.css'       //theme
+import 'primevue/resources/themes/saga-orange/theme.css'       //theme
 import 'primevue/resources/primevue.min.css'            //core css
 import 'primeicons/primeicons.css'                           //icons
+import InputMask from 'primevue/inputmask';
+
+import '/node_modules/primeflex/primeflex.css'
+
 
 
 const app = createApp(App);
 app.use(PrimeVue);
 app.component('Button', Button);
+app.component('InputText', InputText);
+app.component('InputNumber', InputNumber);
+app.component('InputMask', InputMask);
+app.component('pv-dropdown', Dropdown);
 
 app.mount('#app')
 
