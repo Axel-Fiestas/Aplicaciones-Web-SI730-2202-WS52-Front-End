237316de66efbe6395e41a9ce6ea056164fccc3e
Axel Joshue Fiestas Santisteban <josue123452002@gmail.com>
2022-09-18 20:24:46 -0500

    En proceso

--- VUE3/Vue/03-fundamentos/tests/unit/components/indecision.spec.js
+++ VUE3/Vue/03-fundamentos/tests/unit/components/indecision.spec.js
@@ -7,11 +7,21 @@ describe("Indecision Component",()=>{
     let wrapper
     let clgSpy
 
+    global.fetch = jest.fn(()=>Promise.resolve({
+        json: ()=> Promise.resolve({
+            "answer": "yes",
+            "forced": false,
+            "image": "https://yesno.wtf/assets/yes/2.gif"
+        })
+    }))
+
     beforeEach(()=>{
 
         wrapper=shallowMount(Indecision)
 
         clgSpy = jest.spyOn( console , "log")
+
+        jest.clearAllMocks()
 
     })
 
@@ -29,10 +39,20 @@ describe("Indecision Component",()=>{
 
         expect(clgSpy).toHaveBeenCalledTimes(1)
         expect(getAnswerSpy).toHaveBeenCalledTimes(0)
+        //expect(getAnswerSpy).not.toHaveBeenCalled()
     })
 
 
-    test("escribir el simbolo de interrogacion debe disparar el fetch",()=>{
+    test("escribir el simbolo de interrogacion debe disparar el getAnswer",async()=>{
+
+        const getAnswerSpy=jest.spyOn(wrapper.vm,"getAnswer")
+
+        const input=wrapper.find("input")
+        await input.setValue("Ya pero eres o no eres?")
+
+        expect(clgSpy).toHaveBeenCalledTimes(1)
+        expect(getAnswerSpy).toHaveBeenCalled();
+
 
     })
 
