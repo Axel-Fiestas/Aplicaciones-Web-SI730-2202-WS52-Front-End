2d39a75a41442d8ae1bc6fa4c0652a2af9053bc6
Axel Joshue Fiestas Santisteban <josue123452002@gmail.com>
2022-09-25 14:10:29 -0500

    Lazy Load

---
+++ VUE3/Vue/05-rutas-ciclo/src/routes/router.js
@@ -0,0 +1,38 @@
+import {createRouter,createWebHashHistory} from 'vue-router'
+
+// import ListPage from '../modules/pokemon/pages/ListPage.vue'
+// import AboutPage from '../modules/pokemon/pages/AboutPage.vue'
+// import PokemonPage from '../modules/pokemon/pages/PokemonPage.vue'
+// import NoPageFound from '../modules/shared/pages/NoPageFound.vue'
+//Para evitar cargar de golpe todo usaremos Lazy Load o carga perezosa
+
+/*Lazy Load*/ //Una carga inicial pequeña para que las demás páginas
+//vayan cargandose según lo requiera 
+
+const routes = [
+  { 
+    path: '/', 
+    component: () => import(/*webpackChunkName:AboutPage*/'../modules/pokemon/pages/ListPage.vue')
+  },
+  { 
+    path: '/about', 
+    component: ()=> import(/*webpackChunkName:AboutPage*/'../modules/pokemon/pages/AboutPage.vue')
+  },
+  { 
+    path: '/id', 
+    component: ()=>import(/*webPackChunkName:PokemonPage*/'../modules/pokemon/pages/PokemonPage.vue')
+  },
+  { 
+    path: '/:pathMatch(.*)*',
+    component: ()=>import(/*webPackChunkName:NotPageFound*/'../modules/shared/pages/NoPageFound.vue')
+  }
+]
+
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes, 
+})
+
+export default router
+
